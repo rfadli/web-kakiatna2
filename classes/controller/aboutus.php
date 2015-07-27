ec07caddfbdd8da212eb7d463892ca644ddd5160
rfadli <rianfadli1993@yahoo.com>
2015-07-27 14:23:30 +0700

    ok

---
+++ classes/controller/aboutus.php
@@ -0,0 +1,31 @@
+<?php
+
+class aboutus_controller extends controller
+{
+	public function index()
+	{
+		/*$db = Db::init();
+		$content = $db->content;
+		$q = array(
+			'category_content' => new MongoId(SLIDER_ID),
+			'contributor_id' => CLIENT_ID,
+		);
+		$mcontent = $content->find($q);*/
+		
+		//$tw = $this->getLatestTweet();
+		$p = array(
+			'page_header' => "Dashboard",
+			'page_description' => "Dashboard",
+			//'mcontent' => $mcontent,
+			//'lastest_tweet' => $tw['text'],
+			//'lastest_tweet_time' => $tw['time'],
+		);
+		$content = $this->getView(DOCVIEW.'aboutus/index.php', $p);
+		$this->addView('content', $content);
+		
+		//$this->tambah();
+		$this->render(array());
+	}
+	
+	
+}

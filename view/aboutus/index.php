adb64773f5900bc1714005cad2c1b246574b4727
rfadli <rianfadli1993@yahoo.com>
2015-07-29 18:51:19 +0700

    ok

--- view/aboutus/index.php
+++ view/aboutus/index.php
@@ -1,54 +1,4 @@
-<!DOCTYPE html>
-<!--[if lt IE 7 ]><html class="ie ie6" lang="en"> <![endif]-->
-<!--[if IE 7 ]><html class="ie ie7" lang="en"> <![endif]-->
-<!--[if IE 8 ]><html class="ie ie8" lang="en"> <![endif]-->
-<!--[if (gte IE 9)|!(IE)]><!-->
-<html lang="en-US"> <!--<![endif]-->
 
-<!-- head -->
-<head>
-<meta http-equiv="content-type" content="text/html;charset=UTF-8" />
-
-<!-- meta -->
-<meta charset="UTF-8" />
-<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
-<title>Cake</title>
-
-
-<link rel="shortcut icon" href="images/favicon.ico" type="image/x-icon" />	
-
-
-<link rel='stylesheet' id='contact-form-7-css'  href='/public/css/cform.css' type='text/css' media='all' />
-<link rel='stylesheet' id='tp_twitter_plugin_css-css'  href='/public/css/tp_twitter_plugin.css' type='text/css' media='screen' />
-<link rel='stylesheet' id='rs-plugin-settings-css'  href='/public/rs-plugin/css/settings.css' type='text/css' media='all' />
-
-<link rel='stylesheet' id='select2-css'  href='/public/css/select.css' type='text/css' media='all' />
-<link rel='stylesheet' id='woocommerce-layout-css'  href='/public/css/woocommerce-layout.css' type='text/css' media='all' />
-<link rel='stylesheet' id='woocommerce-smallscreen-css'  href='/public/css/woocommerce-smallscreen.css' type='text/css' media='only screen and (max-width: 768px)' />
-<link rel='stylesheet' id='woocommerce-general-css'  href='/public/css/woocommerce.css' type='text/css' media='all' />
-<link rel='stylesheet' id='style-css'  href='/public/css/style.css' type='text/css' media='all' />
-<link rel='stylesheet' id='prettyPhoto-css'  href='/public/css/prettyPhoto.css' type='text/css' media='all' />
-<link rel='stylesheet' id='owl-carousel-css'  href='/public/js/owl-carousel/owl.carousel.css' type='text/css' media='all' />
-<link rel='stylesheet' id='owl-theme-css'  href='/public/js/owl-carousel/owl.theme.css' type='text/css' media='all' />
-<link rel='stylesheet' id='jplayer-css'  href='/public/css/blue.monday/jplayer.blue.monday.css' type='text/css' media='all' />
-<link rel='stylesheet' id='jquery-ui-css'  href='/public/css/ui/jquery.ui.all.css' type='text/css' media='all' />
-<link rel='stylesheet' id='responsive-css'  href='/public/css/responsive.css' type='text/css' media='all' />
-<link rel='stylesheet' id='images-green-css'  href='/public/css/skins/green/images.css' type='text/css' media='all' />
-<link rel='stylesheet' id='style-colors-php-css'  href='/public/css/style-colors.css' type='text/css' media='all' />
-<link rel='stylesheet' id='style-php-css'  href='/public/css/style-2.css' type='text/css' media='all' />
-<link rel='stylesheet' id='mfn-woo-css'  href='/public/css/woocommerce.css' type='text/css' media='all' />
-<link rel="stylesheet" href="/public/css/fonts/mfn-icons.css" media="all" />
-<link rel="stylesheet" href="/public/css/custom.css" media="all" />
-
-<link rel='stylesheet' id='Ubuntu-css'  href='http://fonts.googleapis.com/css?family=Ubuntu%3A100%2C300%2C400%2C400italic%2C700&amp;ver=4.2' type='text/css' media='all' />
-
-
-<!--[if lt IE 9]>
-<script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
-<![endif]-->
-
-
-</head>
 
 <!-- body -->
 <body class="page page-child page-template-default color-custom  with_aside aside_left  layout-full-width header-white header-bg sticky-header">
@@ -172,5 +122,5 @@
 </div>
 
 
-</body>
-</html>
+
+

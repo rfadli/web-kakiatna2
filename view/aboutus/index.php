4849ece19c8a55236b10e9bbc1840a2efacd741e
rfadli <rianfadli1993@yahoo.com>
2015-07-29 15:38:52 +0700

    ok

--- view/aboutus/index.php
+++ view/aboutus/index.php
@@ -15,32 +15,42 @@
 										<li><a href="#-12"><i class="icon-user-add"></i>VISION, MISSION AND VALUES</a></li>
 									</ul>
 									<div id="-10">
-										<?php
-										foreach ($mhistory as $key) 
-										{
-											echo 'dari admin <br />';
-											echo $key['content'];
-										}
-										?>
+										<h5>HISTORY</h5>
+										 Phasellus ultricies ipsum urna, et consequat massa mattis quis. Praesent quis tortor tortor. Phasellus eget elementum nunc, adipiscing malesuada neque. Sed tempus augue vitae ligula tincidunt, a vestibulum justo mattis. Nulla vel leo gravida, congue nibh vitae, auctor ante. Sed a dictum tortor. <br/>
+										<br/>
+										Quisque ac ipsum et orci accumsan dignissim. Cras dignissim urna ut quam facilisis iaculis. Nullam a lacinia sem. Praesent eu viverra mauris. Nullam ut lectus posuere, egestas lorem eget, commodo purus. Vivamus vitae sollicitudin quam. Mauris vel lobortis enim. Nulla facilisi.
 									</div>
 									<div id="-11">
-										<?php
-										foreach ($mwordsceo as $key) 
-										{
-											echo 'dari admin <br />';
-											echo $key['content'];
-										}
-										?>
+										<h5>WORDS FROM OUR CEO</h5>
+										 Ut vestibulum bibendum magna eget varius. Phasellus et urna in justo vestibulum fermentum in non enim. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Proin a felis pharetra, aliquam purus vitae, tristique mauris. Ut consectetur nibh posuere dolor sagittis, id lacinia purus luctus. Nunc tempor neque vitae eros imperdiet cursus. Maecenas consequat diam eget porta tincidunt. <br/><br/>
+										Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Etiam non erat in nibh dignissim porttitor. Proin vestibulum molestie feugiat. Aliquam tincidunt a mi et luctus. Pellentesque egestas lobortis mollis. Curabitur ut nulla sit amet massa consequat dignissim vel in orci. Morbi mollis eros eget arcu rutrum consequat. <br/><br/>
 									</div>
 									<div id="-12">
+										<h5>VISSION</h5>
+										 Our vision is to become a leading diversified business enterprise in Indonesia with the foundation of our values of People Orientation, Integrity, Team Spirit, Loyalty, and Growth & Learning. <br/>
+										<br/>
+										<h5>MISSION</h5>
+										- To provide the maximum benefit for all our stakeholders<br /> 
+										- To create deliberate opportunities and prosperity<br /> 
+										- To deliver the best in our products and services to our customers<br />
+										- To build a challenging and dynamic work environment to our employees<br />
 										
-										<?php
-										foreach ($mvision as $key) 
-										{
-											echo 'dari admin <br />';
-											echo $key['content'];
-										}
-										?>
+										<br/>
+										<h5>VALUES</h5>
+										People Orientation<br />
+										We emphasise our focus on the people aspect of business, the customers and the employees. For us, we dedicate our business to fulfil our customer's need and to improve the life quality of our employees.<br /><br />
+										
+										Integrity <br />
+										We commit ourselves to the ethics in doing a good and clean business, with excellence of good corporate governance in every aspect of our business.<br /><br />
+										
+										Team Spirit<br />
+										We come together as a one cohesive team by engaging ourselves in multiple ways to reach our goals.<br /><br />
+										
+										Loyalty<br />
+										We put the importance of loyalty to both our customers and employees above any interest that might comes in our business.<br /></br />
+										
+										Growth & Learning<br /> 
+										We believe that growth & learning is a process we must adhere in order to build a strong and solid foundation for our enterprise.<br /><br />
 									</div>
 								</div>
 							</div>

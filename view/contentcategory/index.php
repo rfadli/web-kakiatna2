0764626bd1b96ce17f15fb7ac504b08980269cdf
rfadli <rianfadli1993@yahoo.com>
2015-07-29 15:06:07 +0700

    ok

--- view/contentcategory/index.php
+++ view/contentcategory/index.php
@@ -11,8 +11,27 @@
 
 									<!-- blog Item -->
 									<?php
+									$curl = new Curl();
+									
 									foreach ($mcontent as $key) 
 									{
+										$q = array(
+											'id' => trim($key['_id']),
+											'height' => 570,
+											'width' => 1200
+										);
+										
+										$curl->get('http://admin.cms.deboxs.com:8055/api/getimagecontent', $q);
+										$rest = $curl->response;
+										
+										$json = json_decode($rest, TRUE);
+										
+										$url = '';
+										if($json['status'] == "OK")
+										{
+											$url = $json['url'];
+										}
+										
 										echo '<div class="post-item isotope-item clearfix post-519 post type-post status-publish format-standard has-post-thumbnail hentry category-motion tag-css3 tag-framework tag-wordpress">';
 										echo '<div class="post-meta-modern">';
 										echo '<div class="date">';
@@ -25,7 +44,7 @@
 										echo '</div>';
 										echo '<div class="post-photo-wrapper">';
 										echo '<div class="post-photo">';
-										echo '<img width="1200" height="570" src="upload/1-1200x570.jpg" class="scale-with-grid wp-post-image" alt="1"/>';
+										echo '<img src="'.$url.'" class="scale-with-grid wp-post-image" alt="1"/>';
 										echo '</div>';
 										echo '</div>';
 										echo '<div class="post-desc-wrapper">';
@@ -70,324 +89,19 @@
 									
 
 									<!-- blog Item -->
-									<div class="post-item isotope-item clearfix post-507 post type-post status-publish format-standard has-post-thumbnail hentry category-jquery tag-themeforest">
-										<div class="post-meta-modern">
-											<div class="date">
-												<span class="day">17</span><span class="month">February</span><span class="year">2014</span>
-											</div>
-											<div class="button-comments">
-												<a href="#"><span class="icons-wrapper"><i class="icon-comment-empty-fa"></i><i class="icon-comment-fa"></i></span><span class="label">2</span></a>
-											</div>
-											
-										</div>
-										<div class="post-photo-wrapper">
-											<div class="post-photo">
-												<div id="rev_slider_9_2_wrapper" class="rev_slider_wrapper fullwidthbanner-container" style="margin:0px auto;background-color:#E9E9E9;padding:0px;margin-top:0px;margin-bottom:0px;max-height:818px;">
-													<!-- START REVOLUTION SLIDER 4.6.9 fullwidth mode -->
-													<div id="rev_slider_9_2" class="rev_slider fullwidthabanner" style="display:none;max-height:818px;height:818px;">
-														<ul>
-															<!-- SLIDE  -->
-															<li data-transition="slidehorizontal" data-slotamount="7" data-masterspeed="300" data-saveperformance="off">
-															<!-- MAIN IMAGE -->
-															<img src="upload/3.jpg" alt="3" data-bgposition="center top" data-bgfit="cover" data-bgrepeat="no-repeat">
-															<!-- LAYERS -->
-															</li>
-															<!-- SLIDE  -->
-															<li data-transition="slidehorizontal" data-slotamount="7" data-masterspeed="300" data-saveperformance="off">
-															<!-- MAIN IMAGE -->
-															<img src="upload/heart_motion.jpg" alt="heart_motion" data-bgposition="center top" data-bgfit="cover" data-bgrepeat="no-repeat">
-															<!-- LAYERS -->
-															</li>
-														</ul>
-														<div class="tp-bannertimer tp-bottom" >
-														</div>
-													</div>
-												</div>
-												<!-- END REVOLUTION SLIDER -->
-											</div>
-										</div>
-										<div class="post-desc-wrapper">
-											<div class="post-desc">
-												<div class="post-title">
-													<h4><a href="#">Revolution Slider left sidebar</a></h4>
-												</div>
-												<div class="post-meta">
-													<div class="author">
-														By <a href="#">Max Themes</a>
-													</div>
-													<div class="category">
-														In <a href="#" rel="category tag">jQuery</a>
-													</div>
-													<div class="date">
-														February 17, 2014
-													</div>
-													<hr class="hr_narrow hr_left"/>
-												</div>
-												<div class="post-excerpt">
-													<h6>Standard post with Revolution Slider.</h6>
-													<p>
-														Suspendisse turpis et odio. Vestibulum aliquam odio.
-													</p>
-												</div>
-												<div class="post-footer">
-													<a href="detil.html" class="post-more">Read more</a>
-													<div class="button-comments">
-														<a href="#"><span class="icons-wrapper"><i class="icon-comment-empty-fa"></i><i class="icon-comment-fa"></i></span><span class="label">2</span></a>
-													</div>
-													
-												</div>
-											</div>
-										</div>
-									</div>
+									
 
 									<!-- blog Item -->
-									<div class="post-item isotope-item clearfix no-img post-10268 post type-post status-publish format-quote hentry category-print post_format-post-format-quote">
-										<div class="post-meta-modern">
-											<div class="date">
-												<span class="day">16</span><span class="month">February</span><span class="year">2014</span>
-											</div>
-											<div class="button-comments">
-												<a href="#"><span class="icons-wrapper"><i class="icon-comment-empty-fa"></i><i class="icon-comment-fa"></i></span><span class="label">0</span></a>
-											</div>
-											
-										</div>
-										<div class="post-photo-wrapper">
-											<div class="post-photo">
-											</div>
-										</div>
-										<div class="post-desc-wrapper">
-											<div class="post-desc">
-												<div class="post-title">
-													<blockquote>
-														<a href="#">Post format Quote. Sed eu quam justo. Duis condimentum dui id massa dapibus, eget pellentesque est ultricies.</a>
-													</blockquote>
-												</div>
-												<div class="post-meta">
-													<div class="author">
-														By <a href="#">Max Themes</a>
-													</div>
-													<div class="category">
-														In <a href="#" rel="category tag">Print</a>
-													</div>
-													<div class="date">
-														February 16, 2014
-													</div>
-													<hr class="hr_narrow hr_left"/>
-												</div>
-												<div class="post-excerpt">
-													Proin ullamcorper iaculis elit vel faucibus. Suspendisse condimentum quis lectus nec consectetur. Aliquam erat volutpat. Maecenas nec iaculis turpis, eget congue massa. Ut ultrices consectetur eleifend. [&hellip;]
-												</div>
-												<div class="post-footer">
-													<a href="#" class="post-more">Read more</a>
-													<div class="button-comments">
-														<a href="#"><span class="icons-wrapper"><i class="icon-comment-empty-fa"></i><i class="icon-comment-fa"></i></span><span class="label">0</span></a>
-													</div>
-													
-												</div>
-											</div>
-										</div>
-									</div>
+									
 
 									<!-- blog Item -->
-									<div class="post-item isotope-item clearfix post-10284 post type-post status-publish format-video has-post-thumbnail hentry category-jquery category-motion post_format-post-format-video">
-										<div class="post-meta-modern">
-											<div class="date">
-												<span class="day">23</span><span class="month">January</span><span class="year">2014</span>
-											</div>
-											<div class="button-comments">
-												<a href="#"><span class="icons-wrapper"><i class="icon-comment-empty-fa"></i><i class="icon-comment-fa"></i></span><span class="label">0</span></a>
-											</div>
-											
-										</div>
-										<div class="post-photo-wrapper">
-											<div class="post-photo">
-												<div id="jp_container_620" class="jp-video mfn-jcontainer">
-													<div class="jp-type-single">
-														<div id="jquery_jplayer_620" class="jp-jplayer mfn-jplayer" data-m4v="upload/Sequence-01_1.mp4" data-img="upload/6-1200x570.jpg" data-swf="#">
-														</div>
-														<div class="jp-gui">
-															<div class="jp-video-play">
-																<a href="javascript:;" class="jp-video-play-icon" tabindex="1">play</a>
-															</div>
-															<div class="jp-interface">
-																<div class="jp-progress">
-																	<div class="jp-seek-bar">
-																		<div class="jp-play-bar">
-																		</div>
-																	</div>
-																</div>
-																<div class="jp-current-time">
-																</div>
-																<div class="jp-duration">
-																</div>
-																<div class="jp-controls-holder">
-																	<ul class="jp-controls">
-																		<li><a href="javascript:;" class="jp-play" tabindex="1">play</a></li>
-																		<li><a href="javascript:;" class="jp-pause" tabindex="1">pause</a></li>
-																		<li><a href="javascript:;" class="jp-stop" tabindex="1">stop</a></li>
-																		<li><a href="javascript:;" class="jp-mute" tabindex="1" title="mute">mute</a></li>
-																		<li><a href="javascript:;" class="jp-unmute" tabindex="1" title="unmute">unmute</a></li>
-																		<li><a href="javascript:;" class="jp-volume-max" tabindex="1" title="max volume">max volume</a></li>
-																	</ul>
-																	<div class="jp-volume-bar">
-																		<div class="jp-volume-bar-value">
-																		</div>
-																	</div>
-																	<ul class="jp-toggles">
-																		<li><a href="javascript:;" class="jp-full-screen" tabindex="1" title="full screen">full screen</a></li>
-																		<li><a href="javascript:;" class="jp-restore-screen" tabindex="1" title="restore screen">restore screen</a></li>
-																		<li><a href="javascript:;" class="jp-repeat" tabindex="1" title="repeat">repeat</a></li>
-																		<li><a href="javascript:;" class="jp-repeat-off" tabindex="1" title="repeat off">repeat off</a></li>
-																	</ul>
-																</div>
-																<div class="jp-title">
-																	<ul>
-																		<li>jPlayer Video Title</li>
-																	</ul>
-																</div>
-															</div>
-														</div>
-														<div class="jp-no-solution">
-															<span>Update Required</span>To play the media you will need to either update your browser to a recent version or update your <a href="http://get.adobe.com/flashplayer/" target="_blank">Flash plugin</a>
-														</div>
-													</div>
-												</div>
-											</div>
-										</div>
-										<div class="post-desc-wrapper">
-											<div class="post-desc">
-												<div class="post-title">
-													<h4><a href="#">Post format HTML5 Video</a></h4>
-												</div>
-												<div class="post-meta">
-													<div class="author">
-														By <a href="#">Max Themes</a>
-													</div>
-													<div class="category">
-														In <a href="#" rel="category tag">jQuery</a>, <a href="#" rel="category tag">Motion</a>
-													</div>
-													<div class="date">
-														January 23, 2014
-													</div>
-													<hr class="hr_narrow hr_left"/>
-												</div>
-												<div class="post-excerpt">
-													<h6>Phasellus fermentum, metus at sagittis sed, tempus nulla. Integer nibh consectetuer tincidunt enim.</h6>
-													<p>
-														Aenean scelerisque, diam. Nullam vitae wisi placerat sodales. Phasellus pulvinar felis, ullamcorper quam. Nam aliquet quis, convallis nec, eros. Sed ipsum primis in augue.Phasellus ornare non, nulla.
-													</p>
-													<p>
-														Vestibulum aliquam odio. Nunc at sapien. Aliquam eleifend, ligula. Etiam rutrum, enim ac imperdiet sagittis, elit.
-													</p>
-												</div>
-												<div class="post-footer">
-													<a href="#" class="post-more">Read more</a>
-													<div class="button-comments">
-														<a href="#"><span class="icons-wrapper"><i class="icon-comment-empty-fa"></i><i class="icon-comment-fa"></i></span><span class="label">0</span></a>
-													</div>
-													
-												</div>
-											</div>
-										</div>
-									</div>
+									
 
 									<!-- blog Item -->
-									<div class="post-item isotope-item clearfix post-10270 post type-post status-publish format-image has-post-thumbnail hentry category-javascript category-print post_format-post-format-image">
-										<div class="post-meta-modern">
-											<div class="date">
-												<span class="day">22</span><span class="month">January</span><span class="year">2014</span>
-											</div>
-											<div class="button-comments">
-												<a href="#"><span class="icons-wrapper"><i class="icon-comment-empty-fa"></i><i class="icon-comment-fa"></i></span><span class="label">0</span></a>
-											</div>
-											
-										</div>
-										<div class="post-photo-wrapper">
-											<div class="post-photo">
-												<img width="600" height="712" src="upload/8-600x712.jpg" class="scale-with-grid wp-post-image" alt="8"/>
-											</div>
-										</div>
-										<div class="post-desc-wrapper">
-											<div class="post-desc">
-												<div class="post-title">
-													<h4><a href="#">Post format Image. Especially for vertical images.</a></h4>
-												</div>
-												<div class="post-meta">
-													<div class="author">
-														By <a href="#">Max Themes</a>
-													</div>
-													<div class="category">
-														In <a href="#" rel="category tag">Javascript</a>, <a href="#" rel="category tag">Print</a>
-													</div>
-													<div class="date">
-														January 22, 2014
-													</div>
-													<hr class="hr_narrow hr_left"/>
-												</div>
-												<div class="post-excerpt">
-													Quisque id libero nisi. Nullam vitae tincidunt magna. Proin faucibus molestie libero, eu feugiat velit sodales eu. Phasellus ornare risus ipsum, ut dapibus libero lacinia quis. [&hellip;]
-												</div>
-												<div class="post-footer">
-													<a href="#" class="post-more">Read more</a>
-													<div class="button-comments">
-														<a href="#"><span class="icons-wrapper"><i class="icon-comment-empty-fa"></i><i class="icon-comment-fa"></i></span><span class="label">0</span></a>
-													</div>
-													
-												</div>
-											</div>
-										</div>
-									</div>
+									
 									
 									<!-- blog Item -->
-									<div class=" post-item isotope-item clearfix post-379 post type-post status-publish format-video has-post-thumbnail hentry category-javascript category-motion tag-mysql post_format-post-format-video">
-										<div class="post-meta-modern">
-											<div class="date">
-												<span class="day">13</span><span class="month">August</span><span class="year">2013</span>
-											</div>
-											<div class="button-comments">
-												<a href="#"><span class="icons-wrapper"><i class="icon-comment-empty-fa"></i><i class="icon-comment-fa"></i></span><span class="label">0</span></a>
-											</div>
-											
-										</div>
-										<div class="post-photo-wrapper">
-											<div class="post-photo">
-												<iframe class="scale-with-grid" src="http://player.vimeo.com/video/62954028" allowfullscreen>
-												</iframe>
-											</div>
-										</div>
-										<div class="post-desc-wrapper">
-											<div class="post-desc">
-												<div class="post-title">
-													<h4><a href="#">Vimeo with right sidebar</a></h4>
-												</div>
-												<div class="post-meta">
-													<div class="author">
-														By <a href="#">Max Themes</a>
-													</div>
-													<div class="category">
-														In <a href="#" rel="category tag">Javascript</a>, <a href="#" rel="category tag">Motion</a>
-													</div>
-													<div class="date">
-														August 13, 2013
-													</div>
-													<hr class="hr_narrow hr_left"/>
-												</div>
-												<div class="post-excerpt">
-													<h6>Ut pretium magna sed massa luctus iaculis sit amet quis dolor.</h6>
-													<p>
-														Quisque sollicitudin nisl vitae magna pretium, eu commodo lacus auctor. Vestibulum ante ipsum primis in faucibus orci luctus.
-													</p>
-												</div>
-												<div class="post-footer">
-													<a href="#" class="post-more">Read more</a>
-													<div class="button-comments">
-														<a href="#"><span class="icons-wrapper"><i class="icon-comment-empty-fa"></i><i class="icon-comment-fa"></i></span><span class="label">0</span></a>
-													</div>
-													<div class="button-love"></div>
-												</div>
-											</div>
-										</div>
-									</div>
+									
 								</div>
 								<div class="column one pager_wrapper">
 									<div class="pager">

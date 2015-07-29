b403a10ba8b301e868951f8dce06dfcad62d0915
rfadli <rianfadli1993@yahoo.com>
2015-07-29 18:52:12 +0700

    ok

--- view/aboutus/index.php
+++ view/aboutus/index.php
@@ -1,73 +1,7 @@
-
-
-<!-- body -->
-<body class="page page-child page-template-default color-custom  with_aside aside_left  layout-full-width header-white header-bg sticky-header">
-
-<!-- #Wrapper -->
-<div id="Wrapper">
-	<!-- #Action_bar -->
-	<div id="Action_bar">
-		<ul id="menu-action-menu" class="top-bar-menu">
-			<li class="menu-item  current-menu-item page_item current_page_item "><a href="index.html">Home</a></li>
-			<li class="menu-item  "><a href="help.html">Help Center</a></li>
-			<li class="menu-item  "><a href="about-us.html">About us</a></li>
-			<li class="menu-item  "><a href="contact.html">Contact</a></li>
-			<li class="menu-item  menu-item-10792"><a href="#">Buy it</a></li>
-		</ul>
-		<div class="contact_info">
-			<p class="phone">
-				<i class="icon-phone"></i><a href="#">+61 (0) 3 8376 6284</a>
-			</p>
-			<p class="mail">
-				<i class="icon-mail-line"></i><a href="#">noreply@envato.com</a>
-			</p>
-		</div>
-	</div>
-	<!-- #Header -->
-	<header id="Header">
-	<!-- .header_placeholder 4sticky  -->
-	<div class="header_placeholder">
-	</div>
-	<div id="Top_bar">
-		<div class="container">
-			<div class="column one">
-				<!-- .logo -->
-				<div class="logo">
-					<h1><a id="logo" href="home-corporate.html" title="Cake"><img class="scale-with-grid" src="upload/logo_dark.png" alt="Cake"/></a></h1>
-				</div>
-				<!-- .menu_wrapper -->
-				<div class="menu_wrapper">
-					<!-- .header_action_button -->
-					
-					<!-- .header_cart -->
-					
-					<!-- #searchform -->
-					
-
-					<!-- #menu -->
-					<nav id="menu" class="menu-main-menu-container">
-					<ul id="menu-main-menu" class="menu">
-						<li class="menu-item  "><a href="home-corporate.html"><span>Home</span></a></li>
-						<li class="menu-item current-menu-item current_page_item current-menu-ancestor current-menu-parent current_page_parent current_page_ancestor menu-item-has-children"><a href="tabs.html"><span>About Us</span></a></li>
-						<li class="menu-item  "><a href="business.html"><span>Business</span></a></li>
-						<li class="menu-item  "><a href="career.html"><span>Career</span></a></li>
-						<li class="menu-item  "><a href="med.html"><span>Media</span></a></li>
-						<li class="menu-item  "><a href="contact.html"><span>Contact</span></a></li>
-						
-					</ul>
-					</nav><a class="responsive-menu-toggle" href="#"><i class='icon-menu'></i></a>
-				</div>
-			</div>
-		</div>
-	</div>
-
-	</header>
-
-	<!-- #Content -->
-	<div id="Content">
+<div id="Content">
 		<div class="content_wrapper clearfix">
 			<!-- .sections_group -->
-			<div class="sections_group">
+			<div class="sections">
 				
 				<div class="section pad0" >
 					<div class="section_wrapper clearfix">
@@ -81,25 +15,32 @@
 										<li><a href="#-12"><i class="icon-user-add"></i>VISION, MISSION AND VALUES</a></li>
 									</ul>
 									<div id="-10">
-										<h5>HISTORY</h5>
-										 Phasellus ultricies ipsum urna, et consequat massa mattis quis. Praesent quis tortor tortor. Phasellus eget elementum nunc, adipiscing malesuada neque. Sed tempus augue vitae ligula tincidunt, a vestibulum justo mattis. Nulla vel leo gravida, congue nibh vitae, auctor ante. Sed a dictum tortor. <br/>
-										<br/>
-										Quisque ac ipsum et orci accumsan dignissim. Cras dignissim urna ut quam facilisis iaculis. Nullam a lacinia sem. Praesent eu viverra mauris. Nullam ut lectus posuere, egestas lorem eget, commodo purus. Vivamus vitae sollicitudin quam. Mauris vel lobortis enim. Nulla facilisi.
+										<?php
+										foreach ($mhistory as $key) 
+										{
+											echo 'dari admin <br />';
+											echo $key['content'];
+										}
+										?>
 									</div>
 									<div id="-11">
-										<h5>WORDS FROM OUR CEO</h5>
-										 Ut vestibulum bibendum magna eget varius. Phasellus et urna in justo vestibulum fermentum in non enim. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Proin a felis pharetra, aliquam purus vitae, tristique mauris. Ut consectetur nibh posuere dolor sagittis, id lacinia purus luctus. Nunc tempor neque vitae eros imperdiet cursus. Maecenas consequat diam eget porta tincidunt. <br/><br/>
-										Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Etiam non erat in nibh dignissim porttitor. Proin vestibulum molestie feugiat. Aliquam tincidunt a mi et luctus. Pellentesque egestas lobortis mollis. Curabitur ut nulla sit amet massa consequat dignissim vel in orci. Morbi mollis eros eget arcu rutrum consequat. <br/><br/>
+										<?php
+										foreach ($mwordsceo as $key) 
+										{
+											echo 'dari admin <br />';
+											echo $key['content'];
+										}
+										?>
 									</div>
 									<div id="-12">
-										<h5>VISSION</h5>
-										 Phasellus ultricies ipsum urna, et consequat massa mattis quis. Praesent quis tortor tortor. Phasellus eget elementum nunc, adipiscing malesuada neque. Sed tempus augue vitae ligula tincidunt, a vestibulum justo mattis. Nulla vel leo gravida, congue nibh vitae, auctor ante. Sed a dictum tortor. <br/>
-										<br/>
-										<h5>MISSION</h5>
-										Quisque ac ipsum et orci accumsan dignissim. Cras dignissim urna ut quam facilisis iaculis. Nullam a lacinia sem. Praesent eu viverra mauris. Nullam ut lectus posuere, egestas lorem eget, commodo purus. Vivamus vitae sollicitudin quam. Mauris vel lobortis enim. Nulla facilisi. <br/>
-										<br/>
-										<h5>VALUES</h5>
-										Aliquam sodales tortor vitae ipsum. Aliquam nulla. Duis aliquam molestie erat. Ut et mauris vel pede varius sollicitudin. Sed ut dolor nec orci tincidunt interdum. Phasellus ipsum. Nunc tristique tempus lectus
+										
+										<?php
+										foreach ($mvision as $key) 
+										{
+											echo 'dari admin <br />';
+											echo $key['content'];
+										}
+										?>
 									</div>
 								</div>
 							</div>
@@ -116,11 +57,3 @@
 			<!-- .four-columns - sidebar -->
 		</div>
 	</div>
-
-	<!-- #Footer -->
-	
-</div>
-
-
-
-

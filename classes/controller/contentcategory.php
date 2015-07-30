5d77c200aa929df156ed3a0db1766757b5c15948
rfadli <rianfadli1993@yahoo.com>
2015-07-30 13:02:22 +0700

    ok

--- classes/controller/contentcategory.php
+++ classes/controller/contentcategory.php
@@ -5,6 +5,18 @@ class contentcategory_controller extends controller
 	{
 		$id = $_GET['id'];
 		
+		$page = "";
+		if (isset($_GET['page']))
+			$page = $_GET['page'];
+		
+		if(strlen(trim($page)) > 0)
+			$page = intval($page);
+		else
+			$page = 1;
+		
+		$docs_per_page = 3;
+		$skip = (int)($docs_per_page * ($page - 1));
+		
 		$db = Db::init();
 		$content = $db->content;
 		$q = array(
@@ -13,10 +25,17 @@ class contentcategory_controller extends controller
 		);
 		$mcontent = $content->find($q);
 		
+		$limit = 10;
+		$pg = new Pagination();
+		$pg -> pag_url = "/contentcategory/index?id=".$id.'&page=';
+		$pg -> calculate_pages($count, $docs_per_page, $page);
+		
 		$p = array(
 			'page_header' => "Dashboard",
 			'page_description' => "Dashboard",
 			'mcontent' => $mcontent,
+			'pagination' => $pg->Show(),
+			'idx' => (($page-1)*$docs_per_page)+1,
 		);
 		$content = $this->getView(DOCVIEW.'contentcategory/index.php', $p);
 		$this->addView('content', $content);

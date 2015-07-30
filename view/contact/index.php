0c0edc2066cd1aaa1bddd15ab95fe5ea415d94fd
rfadli <rianfadli1993@yahoo.com>
2015-07-30 15:08:30 +0700

    ok

--- view/contact/index.php
+++ view/contact/index.php
@@ -42,7 +42,7 @@
 									Jl.Jend.Sudirman Kav.52-53 <br/>
 									Senayan Kebayoran Baru</h6>
 									<p>
-										<i class="icon-phone"></i><a href="#">021 57947908</a>
+										<i class="icon-phone"></i><a href="#">021 57947908</a><br />
 										<i class="icon-mail"></i><a href="#">info@kakiatna.co.id</a>
 									</p>
 									<?php

bb25ac7eb7cda0dada5701394fb507416eb6a8a3
rfadli <rianfadli1993@yahoo.com>
2015-07-30 13:07:16 +0700

    ok

--- classes/controller/contentcategory.php
+++ classes/controller/contentcategory.php
@@ -23,17 +23,19 @@ class contentcategory_controller extends controller
 			'category_content' => new MongoId($id),
 			'contributor_id' => CLIENT_ID,
 		);
-		$mcontent = $content->find($q);
+		$c = $content->find($q)->limit($docs_per_page)->skip($skip)->sort(array("time_created" => -1));
+		$count = $content->count($q);
+		//$mcontent = $content->find($q);
 		
 		$limit = 10;
 		$pg = new Pagination();
 		$pg -> pag_url = "/contentcategory/index?id=".$id.'&page=';
-		$pg -> calculate_pages($docs_per_page, $page);
+		$pg -> calculate_pages($count, $docs_per_page, $page);
 		
 		$p = array(
 			'page_header' => "Dashboard",
 			'page_description' => "Dashboard",
-			'mcontent' => $mcontent,
+			'mcontent' => $c,
 			'pagination' => $pg->Show(),
 			'idx' => (($page-1)*$docs_per_page)+1,
 		);

8c67e65bee23c13abff27045886722010e9fd3a7
rfadli <rianfadli1993@yahoo.com>
2015-08-04 12:02:22 +0700

    ok

--- view/contentdetil/index.php
+++ view/contentdetil/index.php
@@ -1,17 +1,40 @@
 <div id="Content">
 		<div class="content_wrapper clearfix">
-
 			<!-- .sections_group -->
 			<div class="sections_group">
-				<div id="post-535" class="post-535 post type-post status-publish format-video has-post-thumbnail hentry category-jquery category-print tag-css3 tag-grid tag-html5 tag-zend post_format-post-format-video">
+				<div id="post-379" class="post-379 post type-post status-publish format-video has-post-thumbnail hentry category-javascript category-motion tag-mysql post_format-post-format-video">
 					<div class="section section-post-header">
 						<div class="section_wrapper clearfix">
-							<div class="column one post-photo-wrapper">
-								<div class="post-photo">
-									<iframe class="scale-with-grid" src="http://www.youtube.com/embed/WoJhnRczeNg?wmode=opaque" allowfullscreen="" style="height: 413.6px;">
-									</iframe>
-								</div>
-							</div>
+							<?php
+					    	$curl = new Curl();
+							
+							if(isset($data['_id']))
+							{
+								$q = array(
+									'id' => trim($data['_id']),
+									'height' => 413,
+									'width' => 881,
+									
+								);
+								
+								$curl->get('http://admin.cms.deboxs.com:8055/api/getimagecontent', $q);
+								$rest = $curl->response;
+								
+								$json = json_decode($rest, TRUE);
+								
+								$url = '';
+								if($json['status'] == "OK")
+								{
+									$url = $json['url'];
+								}
+								
+						        echo '<div class="column one post-photo-wrapper">';
+								echo '<div class="post-photo">';
+								echo '<img src="'.$url.'" class="scale-with-grid wp-post-image" alt="1" style="height: 413.6px;">';
+								echo '</div>';
+								echo '</div>';
+							}
+					    	?>
 						</div>
 					</div>
 					<div class="post-wrapper-content">
@@ -19,54 +42,36 @@
 							<div class="section_wrapper clearfix">
 								<div class="column one post-meta">
 									<div class="author">
-										By <a href="#">Max Themes</a>
+										By <a href="#">Admin</a>
 									</div>
-									<div class="category">
-										In <a href="#" rel="category tag">jQuery</a>, <a href="#" rel="category tag">Print</a>
-									</div>
+									<!--div class="category">
+										In <a href="#" rel="category tag">Javascript</a>, <a href="#" rel="category tag">Motion</a>
+									</div-->
 									<div class="date">
-										August 11, 2013
+										<?php
+										echo date("M d,Y",$data['time_created'])
+										?>
 									</div>
 									<hr class="hr_narrow hr_left">
 								</div>
 							</div>
 						</div>
-						<div class="section the_content">
-							<div class="section_wrapper">
-								<div class="the_content_wrapper">
-									<h5>Lorem ipsum dolor sit amet purus consectetuer vulputate vitae, congue tristique. Donec condimentum, pulvinar vulputate in, quam.</h5>
-									<p>
-										Suspendisse turpis et odio. Vestibulum aliquam odio. Nunc at sapien. Aliquam eleifend, ligula. Etiam rutrum, enim ac imperdiet sagittis, elit. Sed ut odio sit amet, tortor. Proin at quam. Phasellus fermentum, metus at sagittis sed, tempus nulla. Integer nibh consectetuer tincidunt enim. Aliquam ultricies ante. Nullam eleifend in, vulputate adipiscing. Nunc tristique lorem. Mauris id nisl. Morbi tellus et ultrices velit eleifend sollicitudin posuere, tempor scelerisque, dui porta ac, felis.
-									</p>
-									<h6>Curabitur arcu sit amet libero hendrerit dolor sit amet tellus. Praesent quis wisi curabitu anso thank:</h6>
-									<ul class="list">
-										<li>Nunc tristique lorem</li>
-										<li>Sed ipsum primis in augue</li>
-									</ul>
-									<p>
-										Aenean scelerisque, diam. Nullam vitae wisi placerat sodales. Phasellus pulvinar felis, ullamcorper quam. Nam aliquet quis, convallis nec, eros. Sed ipsum primis in augue.Phasellus ornare non, nulla. Morbi augue ac ornare dolor sit amet, accumsan quam, dictum ante. Donec non enim. Cras orci. Mauris euismod. Ut molestie justo ipsum dolor sapien pede id nunc. Praesent tortor venenatis consequat. Morbi nibh malesuada ultricies.
-									</p>
-									<p>
-										Ut a nunc. Vivamus est. Suspendisse potenti. In id felis. Vestibulum et magnis dis parturient montes, nascetur ridiculus mus. Nam libero quis felis augue imperdiet sed, ornare lorem. Sed nec odio. Suspendisse eu ante. Quisque neque mauris, consectetuer tellus tincidunt dictum faucibus vestibulum. Cras turpis at nulla. Etiam blandit sed, ornare eu, ullamcorper fringilla, justo nibh rutrum ut, eleifend at, accumsan augue id purus laoreet iaculis scelerisque.
-									</p>
-									<p>
-										Integer faucibus et, congue ac, mattis at, vulputate vitae, velit. Pellentesque eget sapien id lectus. Ut eu nulla in faucibus arcu turpis et magnis dis parturient montes, nascetur ridiculus mus. Mauris aliquet elit. Sed quam ultrices posuere ante eget dolor leo sodales tempor. Quisque at arcu nec elit porta tellus hendrerit feugiat lectus at turpis.
-									</p>
-								</div>
-							</div>
-						</div>
-						<div class="section section-post-footer">
-							<div class="section_wrapper clearfix">
-								<div class="column one post-pager">
-								</div>
-							</div>
-						</div>
+						<?php
+						echo '<div class="section the_content">';
+						echo '<div class="section_wrapper">';
+						echo '<div class="the_content_wrapper">';
+						echo $data['content'];
+						echo '</div>';
+						echo '</div>';
+						echo '</div>';
+						?>
+						
 						<div class="section section-post-about">
 							<div class="section_wrapper clearfix">
 								<div class="column one author-box">
 									<div class="author-box-wrapper">
 										<div class="avatar-wrapper">
-											<img alt="Max Themes" src="upload/gravatar.png" class="avatar avatar-64 photo" height="64" width="64">
+											<img alt="Max Themes" src="/public/upload/gravatar.png" class="avatar avatar-64 photo" height="64" width="64">
 										</div>
 										<div class="desc-wrapper">
 											<h6><a href="#">Max Themes</a></h6>
@@ -82,34 +87,34 @@
 					<div class="section section-post-related">
 						<div class="section_wrapper clearfix">
 							<div class="section-related-adjustment">
-								<div class="column one-third post-related post-507 post type-post status-publish format-standard has-post-thumbnail hentry category-jquery tag-themeforest">
+								<div class="column one-third post-related post-519 post type-post status-publish format-standard has-post-thumbnail hentry category-motion tag-css3 tag-framework tag-wordpress">
 									<a class="photo_mask" href="#">
 									<div class="mask">
 									</div>
-									<span class="button_image more"><i class="icon-link"></i></span><img width="600" height="400" src="upload/3-600x400.jpg" class="scale-with-grid wp-post-image" alt="3"></a>
+									<span class="button_image more"><i class="icon-link"></i></span><img width="600" height="400" src="/public/upload/1-600x400.jpg" class="scale-with-grid wp-post-image" alt="1"></a>
 									<div class="desc">
-										<span class="date"><i class="fa fa-clock-o"></i> February 17, 2014</span>
-										<h6><a href="#">Revolution Slider left sidebar</a></h6>
-									</div>
-								</div>
-								<div class="column one-third post-related post-10268 post type-post status-publish format-quote hentry category-print post_format-post-format-quote">
-									<div class="bq_wrapper">
-										<blockquote>
-											<a href="#">Post format Quote. Sed eu quam justo. Duis condimentum dui id massa dapibus, eget pellentesque est ultricies.</a>
-										</blockquote>
-									</div>
-									<div class="desc">
-										<span class="date"><i class="fa fa-clock-o"></i> February 16, 2014</span>
+										<span class="date"><i class="fa fa-clock-o"></i> February 18, 2014</span>
+										<h6><a href="#">Content Builder for posts</a></h6>
 									</div>
 								</div>
 								<div class="column one-third post-related post-10284 post type-post status-publish format-video has-post-thumbnail hentry category-jquery category-motion post_format-post-format-video">
 									<a class="photo_mask" href="#">
 									<div class="mask">
 									</div>
-									<span class="button_image more"><i class="icon-link"></i></span><img width="600" height="400" src="upload/6-600x400.jpg" class="scale-with-grid wp-post-image" alt="6"></a>
+									<span class="button_image more"><i class="icon-link"></i></span><img width="600" height="400" src="/public/upload/6-600x400.jpg" class="scale-with-grid wp-post-image" alt="6"></a>
 									<div class="desc">
 										<span class="date"><i class="fa fa-clock-o"></i> January 23, 2014</span>
 										<h6><a href="#">Post format HTML5 Video</a></h6>
+									</div>
+								</div>
+								<div class="column one-third post-related post-10270 post type-post status-publish format-image has-post-thumbnail hentry category-javascript category-print post_format-post-format-image">
+									<a class="photo_mask" href="#">
+									<div class="mask">
+									</div>
+									<span class="button_image more"><i class="icon-link"></i></span><img width="600" height="400" src="/public/upload/8-600x400.jpg" class="scale-with-grid wp-post-image" alt="8"></a>
+									<div class="desc">
+										<span class="date"><i class="fa fa-clock-o"></i> January 22, 2014</span>
+										<h6><a href="#">Post format Image. Especially for vertical images.</a></h6>
 									</div>
 								</div>
 							</div>
@@ -119,91 +124,9 @@
 						<div class="section_wrapper clearfix">
 							<div class="column one comments">
 								<div id="comments">
-									<h3 id="comments-title">
-									5 Comments </h3>
-									<ol class="commentlist">
-										<li class="comment byuser comment-author-admin bypostauthor even thread-even depth-1 parent" id="comment-16">
-										<div id="div-comment-16" class="comment-body">
-											<div class="comment-author vcard">
-												<img alt="" src="upload/gravatar.png" class="avatar avatar-64 photo" height="64" width="64"><cite class="fn">admin</cite><span class="says">says:</span>
-											</div>
-											<div class="comment-meta commentmetadata">
-												<a href="#">
-												October 4, 2012 at 1:34 pm</a>
-											</div>
-											<p>
-												Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry’s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.
-											</p>
-										</div>
-										<ul class="children">
-											<li class="comment byuser comment-author-admin bypostauthor odd alt depth-2 parent" id="comment-17">
-											<div id="div-comment-17" class="comment-body">
-												<div class="comment-author vcard">
-													<img alt="" src="upload/gravatar.png" class="avatar avatar-64 photo" height="64" width="64"><cite class="fn">admin</cite><span class="says">says:</span>
-												</div>
-												<div class="comment-meta commentmetadata">
-													<a href="#">
-													October 4, 2012 at 1:34 pm</a>
-												</div>
-												<p>
-													It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
-												</p>
-											</div>
-											<ul class="children">
-												<li class="comment byuser comment-author-admin bypostauthor even depth-3" id="comment-18">
-												<div id="div-comment-18" class="comment-body">
-													<div class="comment-author vcard">
-														<img alt="" src="upload/gravatar.png" class="avatar avatar-64 photo" height="64" width="64"><cite class="fn">admin</cite><span class="says">says:</span>
-													</div>
-													<div class="comment-meta commentmetadata">
-														<a href="#">
-														October 4, 2012 at 1:35 pm</a>
-													</div>
-													<p>
-														Contrary to popular belief, Lorem Ipsum is not simply random text.
-													</p>
-												</div>
-												</li>
-												<!-- #comment-## -->
-											</ul>
-											<!-- .children -->
-											</li>
-											<!-- #comment-## -->
-										</ul>
-										<!-- .children -->
-										</li>
-										<!-- #comment-## -->
-										<li class="comment byuser comment-author-admin bypostauthor odd alt thread-odd thread-alt depth-1" id="comment-19">
-										<div id="div-comment-19" class="comment-body">
-											<div class="comment-author vcard">
-												<img alt="" src="upload/gravatar.png" class="avatar avatar-64 photo" height="64" width="64"><cite class="fn">admin</cite><span class="says">says:</span>
-											</div>
-											<div class="comment-meta commentmetadata">
-												<a href="#">
-												October 4, 2012 at 1:35 pm</a>
-											</div>
-											<p>
-												It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of “de Finibus Bonorum et Malorum” (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, “Lorem ipsum dolor sit amet..”, comes from a line in section 1.10.32.
-											</p>
-										</div>
-										</li>
-										<!-- #comment-## -->
-										<li class="comment even thread-even depth-1" id="comment-20">
-										<div id="div-comment-20" class="comment-body">
-											<div class="comment-author vcard">
-												<img alt="" src="upload/gravatar2.png" class="avatar avatar-64 photo" height="64" width="64"><cite class="fn"><a href="http://muffinhosting.com" rel="external nofollow" class="url">Doris</a></cite><span class="says">says:</span>
-											</div>
-											<div class="comment-meta commentmetadata">
-												<a href="#">
-												October 4, 2012 at 2:08 pm</a>
-											</div>
-											<p>
-												It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout.
-											</p>
-										</div>
-										</li>
-										<!-- #comment-## -->
-									</ol>
+									<p class="nocomments">
+										Comments are closed.
+									</p>
 								</div>
 								<!-- #comments -->
 							</div>
@@ -211,8 +134,6 @@
 					</div>
 				</div>
 			</div>
-
-
 			<!-- .four-columns - sidebar -->
 			<div class="four columns">
 				<div class="widget-area clearfix">
@@ -286,4 +207,4 @@
 				</div>
 			</div>
 		</div>
-	</div>
+</div>

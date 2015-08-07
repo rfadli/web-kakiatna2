6dd5f81a0784233c549c5aead3c253fdc97c896f
rfadli <rianfadli1993@yahoo.com>
2015-08-07 17:20:29 +0700

    ok

--- view/contact/index.php
+++ view/contact/index.php
@@ -27,7 +27,7 @@
 							<div class="widget-area clearfix">
 								<aside id="text-3" class="widget widget_text">
 								<div class="textwidget">
-									<h3>Address</h3>
+									<img width="50" height="50" src="/public/media/Address.png">
 									<hr class="hr_left">
 									<?php
 									echo '<h6>';

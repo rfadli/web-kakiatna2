a14f3b31beaf2bd1d65f28597fae65706a216d19
rfadli <rianfadli1993@yahoo.com>
2015-08-10 13:16:47 +0700

    ok

--- view/media/index.php
+++ view/media/index.php
@@ -9,7 +9,7 @@
 							<div class="column one column_tabs">
 								<div class="jq-tabs tabs_wrapper tabs_vertical tabs_big_icon">
 									<ul>
-										<center><h5>MEDIA</h5></center>
+										<center><h5 style="color:#CD171E">MEDIA</h5></center>
 										<li><a href="#-4"><img width="50" height="50" src="/public/media/News.png"></a></li>
 										<li><a href="#-5"><img width="50" height="50" src="/public/media/Blog.png"></a></li>
 										<li><a href="#-6"><img width="50" height="50" src="/public/media/Press_Release.png"></a></li>

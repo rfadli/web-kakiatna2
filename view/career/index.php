7c41b6c4de0213ff8495b53873a7f95856d52409
rfadli <rianfadli1993@yahoo.com>
2015-08-12 11:49:49 +0700

    ok

--- view/career/index.php
+++ view/career/index.php
@@ -25,7 +25,7 @@
 									<div id="-5">
 										<div class="column one column_accordion">
 											<div class="accordion">
-												<h4 style="color:#CD171E" class="title">Job Vacancy</h4>
+												<h4 style="color:#CD171E" class="title">JOB VACANCY</h4>
 												<div class="mfn-acc accordion_wrapper">
 													<?php
 													$curl = new Curl();

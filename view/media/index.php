344f284708b7d1cab721b6b57277078a53b0c7f2
rfadli <rianfadli1993@yahoo.com>
2015-08-13 19:50:57 +0700

    ok

--- view/media/index.php
+++ view/media/index.php
@@ -10,13 +10,13 @@
 								<div class="jq-tabs tabs_wrapper tabs_vertical tabs_big_icon">
 									<ul>
 										<!--center><h5 style="color:#CD171E">MEDIA</h5></center-->
-										<li><a href="#-4"><img width="50" height="50" src="/public/media/News.png"><br />News</a></li>
+										<li><a href="http://dev.kakiatna.co.id/media/index?&page=2#-4"><img width="50" height="50" src="/public/media/News.png"><br />News</a></li>
 										<li><a href="#-5"><img width="50" height="50" src="/public/media/Blog.png"><br /> Blog</a></li>
 										<li><a href="#-7"><img width="50" height="50" src="/public/media/Press_Kit.png"><br />Press Kit</a></li>
 										<li><a href="#-6"><img width="50" height="50" src="/public/media/Press_Release.png"><br />Press Release</a></li>
 										
 									</ul>
-									<div id="-4" style="border:0">
+									<div id="http://dev.kakiatna.co.id/media/index?&page=2#-4" style="border:0">
 										<?php
 										foreach ($mnews as $key) 
 										{

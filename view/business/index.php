5918fc1f787b109064265485ed8b881f15e1fd16
rfadli <rianfadli1993@yahoo.com>
2015-08-27 11:01:21 +0700

    ok

--- view/business/index.php
+++ view/business/index.php
@@ -74,6 +74,16 @@
 																	<a target="_blank" href="http://www.rumindonesia.com/" title="PT.Retail Usaha Mandiri"><img width="155" height="80" src="/public/subsidiaries/RUM.png" class="scale-with-grid wp-post-image" alt="client_4"/></a>
 																</div>
 																</li>
+																<li class=" last_row">
+																<div class="client_wrapper">
+																	<a target="_blank" href="http://www.rumindonesia.com/" title="PT.Retail Usaha Mandiri"><img width="155" height="80" src="/public/subsidiaries/RUM.png" class="scale-with-grid wp-post-image" alt="client_4"/></a>
+																</div>
+																</li>
+																<li class=" last_row">
+																<div class="client_wrapper">
+																	<a target="_blank" href="http://www.rumindonesia.com/" title="PT.Retail Usaha Mandiri"><img width="155" height="80" src="/public/subsidiaries/RUM.png" class="scale-with-grid wp-post-image" alt="client_4"/></a>
+																</div>
+																</li>
 															</ul>
 														</div>
 													</div>

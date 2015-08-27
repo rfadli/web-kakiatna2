bbca50e6005d45e721ab0b3c35eb5678d577742f
rfadli <rianfadli1993@yahoo.com>
2015-08-27 18:49:06 +0700

    ok

--- view/business/index.php
+++ view/business/index.php
@@ -59,6 +59,7 @@
 																	<a target="_blank" href="http://www.kaningapictures.com/" title="PT.Kaninga Sinema Indonesia"><img width="155" height="80" src="/public/subsidiaries/Kaninga_sinema.png" class="scale-with-grid wp-post-image" alt="client_4"/></a>
 																</div>
 																</li>
+																<!-- -->
 																<li class=" last_row">
 																<div class="client_wrapper">
 																	<a target="_blank" href="http://www.kakiatnalogistik.co.id/" title="PT.Kakiatna Logistik"><img width="155" height="80" src="/public/images/logo/kakiatnalogistik.png" class="scale-with-grid wp-post-image" alt="client_4"/></a>
@@ -79,7 +80,7 @@
 																	<a target="_blank" href="http://www.kakiatnalogistik.co.id/" title="PT.Kakiatna Logistik"><img width="155" height="80" src="/public/images/logo/kakiatnalogistik.png" class="scale-with-grid wp-post-image" alt="client_4"/></a>
 																</div>
 																</li>
-																
+																<!-- -->
 																<li class=" last_row">
 																<div class="client_wrapper">
 																	<a target="_blank" href="http://www.rumindonesia.com/" title="PT.Retail Usaha Mandiri"><img width="155" height="80" src="/public/subsidiaries/RUM.png" class="scale-with-grid wp-post-image" alt="client_4"/></a>

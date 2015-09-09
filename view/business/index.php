22adaf47df970e1ebae49b6e4f549627ee297b06
rfadli <rianfadli1993@yahoo.com>
2015-09-09 12:34:21 +0700

    ok

--- view/business/index.php
+++ view/business/index.php
@@ -153,7 +153,7 @@
 										<br />
 										
 										<h5 style="color:#CD171E">Kakiatna Energi</h5>
-										<img src="/public/subsidiaries/Kakiatna_energi.png" width="200" height="60"><br/>
+										<img src="/public/subsidiaries/Kakiatna_energi.png" width="200" height="90"><br/>
 										 PT. Kakiatna Energi is a private power investment and development company. With the IPP (Independent Power Producer) scheme currently being upthrust by the state electricity company to accelerate more power plant procurement in various parts of Indonesia, Kakiatna Energi aims to be the biggest private power producer in Indonesia.<br/>
 										<div class="post-footer">
 											<a href="http://kakiatnaenergi.co.id/" class="post-more" target="_blank">View website</a>
@@ -164,7 +164,7 @@
 										<br/><br/>
 										
 										<h5 style="color:#CD171E">Nusantara Energi Mandiri</h5>
-										<img src="/public/subsidiaries/Nusantara_Energi_Mandiri.png" width="200" height="60"><br/>
+										<img src="/public/subsidiaries/Nusantara_Energi_Mandiri.png" width="200" height="90"><br/>
 										 PT. Nusantara Energi Mandiri  is an engineering, procurement, and construction company yang secara khusus berkutat di dalam sektor power plant. Didirikan pada tahun 2008, Nusantara Energi Mandiri telah memiliki pengalaman dalam pengembangan PLTA Jatigede bersama dengan Sinohydro Co., Ltd.<br/>
 										<div class="post-footer">
 											<a href="http://kakiatna.co.id/" class="post-more" target="_blank">View website</a>
